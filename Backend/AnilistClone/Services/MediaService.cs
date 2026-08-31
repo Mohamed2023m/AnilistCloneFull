165610ea5f57e4833eab2742099454babca30508
Mohamed <Caanoiyoshaah@proton.me>
2026-08-31 23:16:56 +0200

    changed the amount of media it fetches

--- Backend/AnilistClone/Services/MediaService.cs
+++ Backend/AnilistClone/Services/MediaService.cs
@@ -71,7 +71,7 @@ query ($id: Int) {
         {
             string graphQLQuery = """
 query ($currentPage: Int) {
-  Page(page: $currentPage, perPage: 5) {
+  Page(page: $currentPage, perPage: 20) {
     media(sort: POPULARITY_DESC type: ANIME countryOfOrigin: JP status: RELEASING) {
       id
       coverImage {
